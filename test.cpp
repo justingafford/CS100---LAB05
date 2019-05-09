3f13e5ef1c3fe61046c59bb07bf8f8e03a2f98e9
Vivian Tsai <vtsai006@ucr.edu>
2019-05-08 22:34:38 -0700

    Add the 5 files from lab 2

---
+++ test.cpp
@@ -0,0 +1,35 @@
+#include "c-echo.h"
+ 
+#include "gtest/gtest.h"
+ 
+TEST(EchoTest, HelloWorld) {
+    char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = "world";
+    EXPECT_EQ("hello world", echo(3,test_val));
+}
+
+TEST(EchoTest, EmptyString) {
+    char* test_val[1]; test_val[0] = "./c-echo";
+    EXPECT_EQ("", echo(1,test_val));
+}
+
+TEST(EchoTest, integers) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "123";
+    EXPECT_EQ("123", echo(2,test_val));
+}
+
+TEST(EchoTest, characters) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "!@#$%";
+    EXPECT_EQ("!@#$%", echo(2,test_val));
+}
+
+TEST(EchoTest, DoubleNumbers) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "12.345";
+    EXPECT_EQ("12.345", echo(2,test_val));
+}
+
+ 
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
